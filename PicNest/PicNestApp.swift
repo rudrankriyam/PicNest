eaeb4f2c0d1ea730177b628309267427a9c49509
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-07 14:46:44 +0530

    Add empty project

---
+++ PicNest/PicNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicNestApp.swift
+//  PicNest
+//
+//  Created by Rudrank Riyam on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PicNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
